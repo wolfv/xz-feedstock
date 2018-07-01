f66361ce8d56c0d36565820a008197f40324b71d
Filipe Fernandes <ocefpaf@gmail.com>
2018-07-01 09:51:51 -0300

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX
-make
+./configure --prefix=${PREFIX}  \
+            --build=${BUILD}    \
+            --host=${HOST}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
